5d636418e187336cd97bb778d040558064a9ebe9
Rushilp04 <64269095+Rushilp04@users.noreply.github.com>
2025-01-10 14:44:37 -0500

    Update main.py

--- LinkShortener/mini-link-it-server/app/main.py
+++ LinkShortener/mini-link-it-server/app/main.py
@@ -18,7 +18,6 @@ def hello_world():
 def homepage():
     return render_template('index.html')
 
-
 @app.route('/<path:generatedKey>', methods=['GET'])
 def fetch_from_firebase(generatedKey):
     ref = db.reference("/"+ generatedKey)
